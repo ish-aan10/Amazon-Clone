df9b5f4ccf764980dbc40a46a4a1d71b625a37a2
ish-aan10 <frostcannon10@gmail..com>
2024-07-07 14:46:12 +0530

    order async created

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -2,6 +2,7 @@ import { cart } from "../../data/cart.js";
 import { getProduct } from "../../data/products.js";
 import { getDeliveryOption } from "../../data/delivery-options.js";
 import { formatCurrency } from "../utils/money.js";
+import { addOrder } from "../../data/orders.js";
 
 export function renderPaymentSummary() {
     let productPricePaisa = 0;
@@ -34,8 +35,29 @@ export function renderPaymentSummary() {
                     <div class="payment-summary-money">&#8377; ${formatCurrency(totalPaisa)}</div> 
                 </div>
 
-                <button class="place-order-button button-primary">Place your order</button>
+                <button class="place-order-button button-primary js-place-order">Place your order</button>
     `;
 
     document.querySelector(".js-payment-summary").innerHTML = paymentSummaryHTML;
+
+    document.querySelector(".js-place-order").addEventListener('click', async () => {
+        try {
+            const response = await fetch('https://supersimplebackend.dev/orders', {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    cart: cart
+                })
+            });
+    
+            const order = await response.json();
+            addOrder(order);
+        } catch {
+            console.log("Unexpected Error. Try Again Later.");
+        }
+
+        window.location.href = "orders.html";
+    });
 }
